575b083e3c68bf13ced311b6d77b0c4966320bea
kbdavis07 <kbdavis07@yahoo.com>
2014-05-27 12:56:36 -0400

    More Model adds and changes

--- Jitc/Models/ApplicationDbContext.cs
+++ Jitc/Models/ApplicationDbContext.cs
@@ -17,5 +17,7 @@ namespace Jitc.Models
         public System.Data.Entity.DbSet<Jitc.Models.SentMail> SentMails { get; set; }
         public System.Data.Entity.DbSet<Jitc.Models.Email> Emails { get; set; }
         public System.Data.Entity.DbSet<Jitc.Models.MailingList> MailingLists { get; set; }
+        public System.Data.Entity.DbSet<Jitc.Models.Subscription> Subscriptions { get; set; }
+ 
     }
 }
---
+++ Jitc/Models/Subscription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Jitc.Models
+{
+    /// <summary>
+    /// List of Subscriptions Each User Is subscribed to
+    /// </summary>
+    public partial class Subscription
+    {
+        [Key]
+        public int SubscriptionId { get; set; }
+
+        [Required]
+        public int MailingListID { get; set; }
+
+        [Required]
+        public int MailRecipientID { get; set; }
+
+        public virtual MailRecipient Recipients { get; set; }
+        public virtual MailingList MailingLists { get; set; }
+
+        
+
+    }
+}
