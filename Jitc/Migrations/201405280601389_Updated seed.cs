3808f788c49a8e33eadf420f1384e545ac91c38c
kbdavis07 <kbdavis07@yahoo.com>
2014-05-28 02:07:16 -0400

    More Updates

---
+++ Jitc/Migrations/201405280601389_Updated seed.cs
@@ -0,0 +1,16 @@
+namespace Jitc.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Updatedseed : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Jitc/Migrations/Configuration.cs
+++ Jitc/Migrations/Configuration.cs
@@ -1,31 +1,69 @@
 namespace Jitc.Migrations
 {
+    using Jitc.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Jitc.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(Jitc.Models.ApplicationDbContext context)
-        {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+        protected override void Seed(ApplicationDbContext context)
+        {
+            this.AddUserAndRoles();
+        }
+
+
+        bool AddUserAndRoles()
+        {
+            bool success = false;
+
+            var idManager = new IdentityManager();
+            success = idManager.CreateRole("Admin");
+            if (!success == true) return success;
+
+            success = idManager.CreateRole("CanEdit");
+            if (!success == true) return success;
+
+            success = idManager.CreateRole("User");
+            if (!success) return success;
+
+            success = idManager.CreateRole("Test");
+            if (!success) return success;
+
+            success = idManager.CreateRole("TestII");
+            if (!success) return success;
+
+            var newUser = new ApplicationUser()
+            {
+                UserName = "Kbdavis07",
+                FirstName = "Brian",
+                LastName = "Davis",
+                Email = "Kbdavis07@yahoo.com"
+            };
+
+            // Be careful here - you  will need to use a password which will 
+            // be valid under the password rules for the application, 
+            // or the process will abort:
+            success = idManager.CreateUser(newUser, "Password1");
+            if (!success) return success;
+
+            success = idManager.AddUserToRole(newUser.Id, "Admin");
+            if (!success) return success;
+
+            success = idManager.AddUserToRole(newUser.Id, "CanEdit");
+            if (!success) return success;
+
+            success = idManager.AddUserToRole(newUser.Id, "User");
+            if (!success) return success;
+
+            return success;
         }
     }
 }
