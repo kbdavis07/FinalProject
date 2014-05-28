cae9d77f8f62019baecaa2cb00fc7a8d5d0469bd
kbdavis07 <kbdavis07@yahoo.com>
2014-05-28 01:12:21 -0400

    Added QRTZ Tables to database

--- Jitc/Global.asax.cs
+++ Jitc/Global.asax.cs
@@ -1,21 +1,44 @@
-﻿using System;
+﻿using Quartz;
+using Quartz.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Common.Logging;
+
+
 
 namespace Jitc
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static IScheduler Scheduler;
+
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ISchedulerFactory sf = new StdSchedulerFactory();
+            Scheduler = sf.GetScheduler();
+
+            Scheduler.Start();
         }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            Scheduler.Shutdown();
+        }
+
+
+
+
+
     }
 }
