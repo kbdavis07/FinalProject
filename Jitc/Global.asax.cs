aaa60d9b1d3b5cc1e4e989f256d297526ca08554
kbdavis07 <kbdavis07@yahoo.com>
2014-05-28 07:35:06 -0400

    Added Elmah

--- Jitc/Global.asax.cs
+++ Jitc/Global.asax.cs
@@ -20,24 +20,68 @@ namespace Jitc
 
         protected void Application_Start()
         {
+
+            Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException("Application Start"));
+  
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            ISchedulerFactory sf = new StdSchedulerFactory();
-            Scheduler = sf.GetScheduler();
+            try
+            {
+                ISchedulerFactory sf = new StdSchedulerFactory();
+                Scheduler = sf.GetScheduler();
 
-            Scheduler.Start();
+                Scheduler.Start();
+                Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException("Scheduler Started Successfully"));
+            }
+
+            catch (SchedulerException se)
+            {
+               string schedulerError = se.ToString();
+               Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException(schedulerError));
+            }
+        
+        
         }
 
         protected void Application_End(object sender, EventArgs e)
         {
-            Scheduler.Shutdown();
+
+            Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException("Application End"));
+
+          try
+          {
+              Scheduler.Shutdown();
+              Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException("Scheduler ShutDown Successfully"));
+          }
+
+          catch (SchedulerException se)
+          {
+              string schedulerError = se.ToString();
+              Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException(schedulerError));
+          }  
+        
         }
 
 
+       protected void Application_Error(object sender, EventArgs e)
+       {
+           Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException("Application Error:" + e));
+       }
 
+
+       //protected void Application_BeginRequest(object sender, EventArgs e)
+       //{
+
+       //    Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException("BeginRequest"));
+       //}
+
+       // protected void Application_EndRequest(object sender, EventArgs e)
+       //{
+       //    Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException("EndRequest"));
+       //}
 
 
     }
---
+++ Jitc/Jobs/XmlJob.cs
@@ -0,0 +1,78 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Xml;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using System.Web.Mvc;
+using Jitc.Models;
+using Jitc.Controllers;
+using System.Xml.Linq;
+
+
+namespace Jitc.Jobs
+{
+    public class XmlJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+            {
+
+                Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException("Hello from XmlJob"));
+
+                var timenow = DateTime.Now;
+
+                Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException("It is:" + timenow));
+            
+            try 
+                    {
+                     
+                     //XmlTextWriter xmlwriter = new XmlTextWriter(HttpContext.Server.MapPath("~/App_Data/Employee.xml"), Encoding.UTF8);
+
+                        var billingData = XDocument.Load(HttpContext.Current.Server.MapPath("~/App_Data/Employee.xml"));
+
+
+                        billingData.Root.Add(new XElement("item", new XElement("id", "ID"),
+
+                            new XElement("customer", "Customer"),
+                            new XElement("type", "Type"),
+                            new XElement("hours", "5")));
+
+
+                        billingData.Save(HttpContext.Current.Server.MapPath("~/App_Data/Billings.xml"));
+
+
+                        //xmlwriter.Formatting = Formatting.Indented;
+                        //xmlwriter.WriteStartDocument();
+                        //xmlwriter.WriteStartElement("Employees");
+                        //xmlwriter.WriteStartElement("Employee");
+                        //xmlwriter.WriteAttributeString("type", "Permanent");
+                        //xmlwriter.WriteElementString("ID", "100");
+                        //xmlwriter.WriteElementString("FirstName", "From Controler");
+                        //xmlwriter.WriteElementString("LastName", "Babu");
+                        //xmlwriter.WriteElementString("Dept", "IT");
+                        //xmlwriter.WriteEndElement();
+                        //xmlwriter.WriteEndElement();
+                        //xmlwriter.WriteEndDocument();
+                        //xmlwriter.Flush();
+                        //xmlwriter.Close();
+
+                    }
+
+
+
+                    catch (SchedulerException se)
+                    {
+                        string schedulerError = se.ToString();
+                        Elmah.ErrorSignal.FromCurrentContext().Raise(new ApplicationException(schedulerError));
+                    }  
+            
+            
+            }
+   }
+
+ 
+}
