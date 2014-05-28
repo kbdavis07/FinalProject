d180669893c123e02439313ee04f677aa2a78371
kbdavis07 <kbdavis07@yahoo.com>
2014-05-28 02:11:58 -0400

    EF for Quartz

---
+++ Jitc/DAL/EF/QRTZ_JOB_DETAILS.cs
@@ -0,0 +1,51 @@
+namespace Jitc.DAL.EF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class QRTZ_JOB_DETAILS
+    {
+        public QRTZ_JOB_DETAILS()
+        {
+            QRTZ_TRIGGERS = new HashSet<QRTZ_TRIGGERS>();
+        }
+
+        [Key]
+        [Column(Order = 0)]
+        [StringLength(100)]
+        public string SCHED_NAME { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        [StringLength(150)]
+        public string JOB_NAME { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        [StringLength(150)]
+        public string JOB_GROUP { get; set; }
+
+        [StringLength(250)]
+        public string DESCRIPTION { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string JOB_CLASS_NAME { get; set; }
+
+        public bool IS_DURABLE { get; set; }
+
+        public bool IS_NONCONCURRENT { get; set; }
+
+        public bool IS_UPDATE_DATA { get; set; }
+
+        public bool REQUESTS_RECOVERY { get; set; }
+
+        [Column(TypeName = "image")]
+        public byte[] JOB_DATA { get; set; }
+
+        public virtual ICollection<QRTZ_TRIGGERS> QRTZ_TRIGGERS { get; set; }
+    }
+}
